5d27ee5a396c52dc42823c7520ac0301c8aa418e
DuckFromLake <121292875+DuckFromLake@users.noreply.github.com>
2023-10-21 23:57:03 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,140 @@ namespace FrameLab9
 {
     public partial class Form1 : Form
     {
+        string name = @"D:\Users\drall\source\repos\FrameLab9\binary", namecopy = @"D:\Users\drall\source\repos\FrameLab9\binarycopy";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int min = 99, max = 0, d;
+            Random random = new Random();
+            
+            int a = Convert.ToInt32(textBox1.Text);
+
+            FileStream f = new FileStream(name, FileMode.OpenOrCreate);
+            f.SetLength(0);
+
+            for (int i = 0; i < a; i++)
+            {
+                f.WriteByte((byte)(random.Next(0, 99)));
+            }
+            f.Seek(0, SeekOrigin.Begin);
+            label2.Text = "";
+            for (long i = 0; i < f.Length; i++)
+            {
+                label2.Text += (f.ReadByte() + " ");
+            }
+
+            f.Seek(0, SeekOrigin.Begin);
+            for (long i = 0; i < f.Length; i++)
+            {
+                d = (int)f.ReadByte();
+                if (d < min) { min = d; }
+                if (d > max) { max = d; }
+            }
+            label5.Text = Convert.ToString(min + max);
+
+            label12.Text = Convert.ToString(File.GetCreationTime(name));
+            label13.Text = Convert.ToString(File.GetLastWriteTime(name));
+            label14.Text = Convert.ToString(File.GetLastAccessTime(name));
+            f.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream f = new FileStream(name, FileMode.OpenOrCreate);
+            FileStream f2 = new FileStream(namecopy, FileMode.OpenOrCreate);
+            f2.SetLength(0);
+
+            f.CopyTo(f2);
+            f2.Seek(0, SeekOrigin.Begin);
+
+            label7.Text = "";
+            for (long i = 0; i < f2.Length; i++)
+            {
+                label7.Text += ((int)f2.ReadByte() + " ");
+            }
+
+            label12.Text = Convert.ToString(File.GetCreationTime(name));
+            label13.Text = Convert.ToString(File.GetLastWriteTime(name));
+            label14.Text = Convert.ToString(File.GetLastAccessTime(name));
+
+            f.Close();
+            f2.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int min = 99, max = 0, d;
+            FileStream f = new FileStream(name, FileMode.OpenOrCreate);
+            Random random = new Random();
+            int x = Convert.ToInt32(textBox2.Text);
+            int number = random.Next(0, (int)f.Length);
+
+            
+            f.Seek(number, SeekOrigin.Begin);
+            f.WriteByte((byte)(x));
+            f.Seek(0, SeekOrigin.Begin);
+            label2.Text = "";
+            for (long i = 0; i < f.Length; i++)
+            {
+                label2.Text += ((int)f.ReadByte() + " ");
+            }
+
+            f.Seek(0, SeekOrigin.Begin);
+            for (long i = 0; i < f.Length; i++)
+            {
+                d = (int)f.ReadByte();
+                if (d < min) { min = d; }
+                if (d > max) { max = d; }
+            }
+            label5.Text = Convert.ToString(min + max);
+
+            label12.Text = Convert.ToString(File.GetCreationTime(name));
+            label13.Text = Convert.ToString(File.GetLastWriteTime(name));
+            label14.Text = Convert.ToString(File.GetLastAccessTime(name));
+            f.Close();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream f = new FileStream(name, FileMode.OpenOrCreate);
+            FileStream f2 = new FileStream(namecopy, FileMode.OpenOrCreate);
+            int min = 99, max = 0, d;
+
+            label2.Text = "";
+            for (long i = 0; i < f.Length; i++)
+            {
+                label2.Text += ((int)f.ReadByte() + " ");
+            }
+
+            label7.Text = "";
+            for (long i = 0; i < f2.Length; i++)
+            {
+                label7.Text += ((int)f2.ReadByte() + " ");
+            }
+
+            f.Seek(0, SeekOrigin.Begin);
+            for (long i = 0; i < f.Length; i++)
+            {
+                d = (int)f.ReadByte();
+                if (d < min) { min = d; }
+                if (d > max) { max = d; }
+            }
+            label5.Text = Convert.ToString(min+max);
+
+            label12.Text = Convert.ToString(File.GetCreationTime(name));
+            label13.Text = Convert.ToString(File.GetLastWriteTime(name));
+            label14.Text = Convert.ToString(File.GetLastAccessTime(name));
+            f.Dispose();
+            f2.Dispose();
+            f.Close();
+            f2.Close();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.IO.Pipes;
 
 namespace FrameLab9
 {
@@ -30,77 +31,115 @@ namespace FrameLab9
                             Console.Clear();
                             int v1 = -1;
                             string sn;
-                            string name = @"D:\Users\drall\source\repos\FrameLab9\binary";
+                            string name = @"D:\Users\drall\source\repos\FrameLab9\binary", namecopy = @"D:\Users\drall\source\repos\FrameLab9\binarycopy";
                             int a;
                             Random random = new Random();
                             Console.WriteLine("Сколько чисел записать в файл?");
                             s1 = Console.ReadLine();
                             a = Convert.ToInt32(s1);
-                            BinaryWriter fs1 = new BinaryWriter(File.Open(name, FileMode.OpenOrCreate));
+
+                            FileStream f = new FileStream(name, FileMode.OpenOrCreate);
+                            f.SetLength(0);
+
                             for (int i = 0; i < a; i++)
                             {
-                                fs1.Write(random.Next(0, 99));
+                                f.WriteByte((byte)(random.Next(0, 99)));
                             }
-                            fs1.Close();
+                            f.Close();
+
                             while (v1 != 0)
                                 {
                                 Console.Clear();
-                                BinaryReader fs2 = new BinaryReader(File.Open(name, FileMode.OpenOrCreate));
-                                while (fs2.PeekChar() > -1)
+                                f = new FileStream(name, FileMode.OpenOrCreate);
+                                f.Seek(0, SeekOrigin.Begin);
+                                for (long i = 0; i < f.Length; i++ )
                                 {
-                                    Console.Write(fs2.ReadInt32() + " ");
+                                    Console.Write((int)f.ReadByte() + " ");
                                 }
+                                f.Close();
+
                                 Console.WriteLine("\n");
-                                fs2.Close();
-                                Console.WriteLine("1) Найти наименьшее и наибольшее значение в файле\n2) Задание 2\n3) Задание 3 \n\n0) Выход \n\nВведите номер метода : ");
-                                    sn = Console.ReadLine();
+                                Console.WriteLine("1) Найти сумму наименьшего и наибольшего значения в файле\n2) Добавить число на случаенную позицию\n3) Скопировать файл\n4) Получить информацию о файле\n\n0) Выход \n\nВведите номер метода : ");
+                                    sn = Console.ReadLine(); 
                                     v1 = Convert.ToInt32(sn);
                                     switch (v1)
                                     {
                                         case 1:
                                             {
                                             Console.Clear();
-                                            int min = 99, max = 0, d; 
-                                            fs2 = new BinaryReader(File.Open(name, FileMode.OpenOrCreate));
-                                            while (fs2.PeekChar() > -1)
+                                            int min = 99, max = 0, d;
+
+                                            f = new FileStream(name, FileMode.OpenOrCreate);
+                                            f.Seek(0, SeekOrigin.Begin);
+                                            for (long i = 0; i < f.Length; i++)
                                             {
-                                                Console.Write(fs2.ReadInt32() + " ");
+                                                Console.Write((int)f.ReadByte() + " ");
                                             }
                                             Console.WriteLine("\n");
-
-                                            while (fs2.PeekChar() > -1)
+                                            f.Seek(0, SeekOrigin.Begin);
+                                            for (long i = 0; i < f.Length; i++)
                                             {
-                                                d = fs2.ReadInt32();
+                                                d = (int)f.ReadByte();
                                                 if (d < min) { min = d; }
                                                 if (d > max) { max = d; }
                                             }
-                                            fs2.Close();
-                                            Console.Write(min + " " + max + "\n");
+                                            f.Close();
+
+                                            Console.WriteLine("Cумма наибольшего и наименьшего: " + (max + min));
 
                                             Console.WriteLine("Нажмите любую кнопку для продолжения...");
-                                                s1 = Console.ReadLine();
-                                                break;
+                                            s1 = Console.ReadLine();
+                                            break;
                                             }
                                         case 2:
                                             {
-                                                Console.Clear();
+                                            Console.Clear();
+                                            Console.WriteLine("Какое значение вы хотите добавить в ряд?");
+                                            s1 = Console.ReadLine();
+                                            int x = Convert.ToInt32(s1);
+                                            int number = random.Next(0, a);
 
-                                                Console.WriteLine("Нажмите любую кнопку для продолжения...");
-                                                s1 = Console.ReadLine();
-                                                break;
+                                            f = new FileStream(name, FileMode.OpenOrCreate);
+                                            f.Seek(number, SeekOrigin.Begin);
+                                            f.WriteByte((byte)(x));
+                                            f.Close();
+                                            Console.WriteLine("Нажмите любую кнопку для продолжения...");
+                                            s1 = Console.ReadLine();
+                                            break;
                                             }
                                         case 3:
                                             {
-                                                Console.Clear();
+                                            Console.Clear();
+                                            f = new FileStream(name, FileMode.OpenOrCreate);
+                                            FileStream f2 = new FileStream(namecopy, FileMode.OpenOrCreate);
+                                            f2.SetLength(0);
 
-                                                Console.WriteLine("Нажмите любую кнопку для продолжения...");
-                                                s1 = Console.ReadLine();
-                                                break;
-
-                                            }
-                                        case 0:
+                                            f.CopyTo(f2);
+                                            f2.Seek(0, SeekOrigin.Begin);
+                                            Console.WriteLine("Содержание файла-копии:");
+                                            for (long i = 0; i < f2.Length; i++)
                                             {
-                                            fs1.Close();
+                                                Console.Write((int)f2.ReadByte() + " ");
+                                            }
+                                            Console.WriteLine("\n");
+                                            f.Close();
+                                            f2.Close();
+                                            Console.WriteLine("Нажмите любую кнопку для продолжения...");
+                                            s1 = Console.ReadLine();
+                                            break;
+                                            }
+                                    case 4:
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine("Время создания файла: " + File.GetCreationTime(name));
+                                            Console.WriteLine("Время последней записи в файл: " + File.GetLastWriteTime(name));
+                                            Console.WriteLine("Время последнего доступа к файлу: " + File.GetLastAccessTime(name));
+                                            Console.WriteLine("Нажмите любую кнопку для продолжения...");
+                                            s1 = Console.ReadLine();
+                                            break;
+                                        }
+                                    case 0:
+                                            {
                                             Console.Clear();
                                                 break;
                                             }
